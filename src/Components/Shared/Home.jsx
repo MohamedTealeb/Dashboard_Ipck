29c5c53b795d0c7742844512150f97e46e74b7df
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-04-19 22:41:58 +0200

    Home page

--- src/Components/Shared/Home.jsx
+++ src/Components/Shared/Home.jsx
@@ -5,9 +5,10 @@ export default function Home() {
   return <>
   <div className="relative">
       <div className="absolute inset-0">
-        <div className="p-8 ml-30">
-          <h2 className='text-black text-2xl font-semibold'>mooooo</h2>
-          {/* Add your products content here */}
+        <div className="p-8 flex gap-4 ml-53">
+          <h2 className='text-black text-2xl font-semibold'>Admins: <span className='text-black'>8</span></h2>
+          <h2 className='text-black text-2xl font-semibold'>Users : <span className='text-black'>8</span></h2>
+         
         </div>
       </div>
       <Sidebar />
