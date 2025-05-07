23070250ccbc0c8ab5c2f623375e06bb81108e18
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-05-07 16:06:57 +0300

    verify

--- src/Modules/Verfiy/Verify.jsx
+++ src/Modules/Verfiy/Verify.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
-import Sidebar from '../../Components/Shared/Sidebar'
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import axios from 'axios';
+
 
 export default function Verify() {
+  const [userVerifications, setUserVerifications] = React.useState([]);
+ React.useEffect(()=>{
+  const getallusersverified = async () => {
+    try {
+     const response = await axios.get(`${import.meta.env.VITE_BASEURL}/verification`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+     setUserVerifications(response.data.verifications);
+    } catch (err) {
+      console.log("Failed to verify");
+    } 
+  };
+  getallusersverified();
+ },[])
+
+  
   return <>
-   
+ 
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 850, marginTop: 20 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">productName</TableCell>
+            <TableCell align="right">imageCover</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {userVerifications.map((user) => (
+            <TableRow
+              key={user.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {user.userId.firstName} {user.userId.lastName}
+              </TableCell>
+              <TableCell align="right">{user.productId.name}</TableCell>
+              <TableCell align="right">
+                <div className="flex justify-end">
+                  <img
+                    src={`${import.meta.env.VITE_IMAGEURL}/${user.verificationImage}`}
+                    alt={`${user.userId.firstName} ${user.userId.lastName} Verification Image`}
+                    className="w-24 h-24 object-contain rounded-md shadow-md"
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
   </>
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { Navigate } from "react-router-dom";
 import authReducer from "../redux/slices/AuthSlices/AuthReducer.jsx";
 import userReducer from "../redux/slices/UserSlices/UserReducer";
 import productReducer from "../redux/slices/ProductSlice/Products.jsx";
 import categoryReducer from "../redux/slices/CategorySlice/Category.jsx";
-
+// import  VerifyReducer  from '../redux/slices/VerfySlice/Verify.jsx';
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     product: productReducer,
     category: categoryReducer,
+    // verify: VerifyReducer
+
   },
 });
-// (function initalizedAuth(){
-//     const token =localStorage.getItem("token");
-//     if (data.user.role === 'admin') {
-//         localStorage.setItem("token", data.access_token);
-
-//         if (window.location.pathname === "/") {
-//             navigate("/home");
-//         } else {
-//             Navigate(window.location.pathname);
-//         }
-//     } else {
-//         setErrorMsg("You are not authorized to access the admin area.");
-//     }
-// })()
 
 export default store;
